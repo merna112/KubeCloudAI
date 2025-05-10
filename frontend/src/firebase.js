4f06acfdb1939f5a3f447d000a013562d4a17d45
merna112 <mernaadel182@gmail.com>
2025-05-10 22:20:33 +0300

    refactor: update firebase configuration to use environment variables for security

--- frontend/src/firebase.js
+++ frontend/src/firebase.js
@@ -1,16 +1,14 @@
 import { initializeApp } from "firebase/app";
-import { getAuth, GoogleAuthProvider } from "firebase/auth";
+import { getAuth } from "firebase/auth"; 
 
 const firebaseConfig = {
-  apiKey: "AIzaSyDIs5XHXDjaFAfZU7d8TYgg-j7IZZG0JrE",
-  authDomain: "fullstack-blog-9b25f.firebaseapp.com",
-  projectId: "fullstack-blog-9b25f",
-  storageBucket: "fullstack-blog-9b25f.appspot.com",
-  messagingSenderId: "906951963054",
-  appId: "1:906951963054:web:af8c53d07ba2c88b6a983f"
+  apiKey: "AIzaSyDIs5XHXDjaFAfZU7d8TYgg-j7IZZG0JrE", 
+  authDomain: "fullstack-blog-9b25f.firebaseapp.com", 
+  projectId: "fullstack-blog-9b25f",               
+  storageBucket: "fullstack-blog-9b25f.appspot.com",  
+  messagingSenderId: "906951963054",               
+  appId: "1:906951963054:web:af8c53d07ba2c88b6a983f" 
 };
 
 const app = initializeApp(firebaseConfig);
 export const auth = getAuth(app);
-export const provider = new GoogleAuthProvider();
-
