1153c10e17e2c9b26750b6e80d2021d5076f529c
merna112 <mernaadel182@gmail.com>
2025-05-11 09:41:29 +0300

    Fix Firebase import/export for OAuth and ensure auth is exported

--- frontend/src/firebase.js
+++ frontend/src/firebase.js
@@ -1,17 +1,19 @@
+// Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-import { getAuth } from "firebase/auth"; 
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
 
+// Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyDIs5XHXDjaFAfZU7d8TYgg-j7IZZG0JrE", 
-  authDomain: "fullstack-blog-9b25f.firebaseapp.com", 
-  projectId: "fullstack-blog-9b25f",               
-  storageBucket: "fullstack-blog-9b25f.appspot.com",  
-  messagingSenderId: "906951963054",               
-  appId: "1:906951963054:web:af8c53d07ba2c88b6a983f" 
+  apiKey: "AIzaSyDts5XHxDjaFAFZU7d8TYgg-j7fZZG0JrE",
+  authDomain: "fullstack-blog-9b25f.firebaseapp.com",
+  projectId: "fullstack-blog-9b25f",
+  storageBucket: "fullstack-blog-9b25f.appspot.com",
+  messagingSenderId: "906951963054",
+  appId: "1:906951963054:web:af8c53d07ba2c88b6a983f"
 };
 
+// Initialize Firebase
 const app = initializeApp(firebaseConfig);
-export const auth = getAuth(app);
-
 
 export default app;
