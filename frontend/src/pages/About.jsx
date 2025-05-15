be9d55df48ee7e9b4be086561e0156d1d6b39b3a
merna112 <mernaadel182@gmail.com>
2025-05-15 23:24:23 +0300

    footer removed

--- frontend/src/pages/About.jsx
+++ frontend/src/pages/About.jsx
@@ -63,12 +63,6 @@ export default function About() {
           </div>
         </div>
       </main>
-
-      <footer className='bg-gray-100 dark:bg-gray-800 py-4 sm:py-6'>
-        <div className='max-w-6xl mx-auto px-4 sm:px-6 text-center text-gray-600 dark:text-gray-400'>
-          © {new Date().getFullYear()} KubeCloudAI. All rights reserved.
-        </div>
-      </footer>
     </div>
   );
 }
